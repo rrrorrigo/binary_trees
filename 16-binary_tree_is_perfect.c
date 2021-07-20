b5f4c2bd20899369b38a1aff23184740b4a0a58d
RaimundoGallino <raigallino0@gmail.com>
2021-07-20 19:18:43 -0300

    Uploading 16

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+/**
+ * binary_tree_height - traverse recursively each level and return the depth
+ *
+ * @tree: pointer to tree
+ * Return: length of printed tree after process
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	int r;
+
+	if (!tree)
+		return (0);
+    if (tree->left)
+        r = binary_tree_is_perfect(tree->left);
+    if (tree->right)
+        r = binary_tree_is_perfect(tree->right);
+    if (r == 0)
+        return (0);
+    if (tree->left && tree->right)
+	    return (1);
+    else
+	    return (0);
+}
