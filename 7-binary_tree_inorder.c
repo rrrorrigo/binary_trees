a2135a197a03984d2fc93b51d552a27c19138635
RaimundoGallino <69832412+RaimundoGallino@users.noreply.github.com>
2021-07-21 20:05:17 -0300

    Correcting task 7

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -10,7 +10,7 @@
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (!tree || !func)
-		return NULL;
+		return;
 	if (tree->left)
 	{
 		binary_tree_inorder(tree->left, func);
