7b95ae632f34c63c2946115a36e2945fadba119f
Rodrigo <rodrigobriones54@gmail.com>
2021-07-21 23:04:06 +0000

    added border case

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -7,9 +7,12 @@
  */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-	func(tree->n);
-	if (tree->left)
-		binary_tree_preorder(tree->left, func);
-	if (tree->right)
-		binary_tree_preorder(tree->right, func);
+	if (tree && func)
+	{
+		func(tree->n);
+		if (tree->left)
+			binary_tree_preorder(tree->left, func);
+		if (tree->right)
+			binary_tree_preorder(tree->right, func);
+	}
 }
