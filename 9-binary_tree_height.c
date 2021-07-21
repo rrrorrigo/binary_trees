f955900ff718582516bd48e5454d5215a025a15a
Rodrigo <rodrigobriones54@gmail.com>
2021-07-21 23:18:21 +0000

    fixed

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -13,8 +13,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t height_r;
 
 	if (!tree)
-		return (NULL);
-
+		return (0);
 	if (tree->left)
 		height_l = 1 + binary_tree_height(tree->left);
 	else
