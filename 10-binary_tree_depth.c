367258540dfe9c31749b6b454b39a0ad2276f9f4
Rodrigo <rodrigobriones54@gmail.com>
2021-07-21 23:08:49 +0000

    added border case

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -11,7 +11,8 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	size_t depth = 0;
 
-	if (tree->parent)
-		depth = 1 + binary_tree_depth(tree->parent);
+	if (tree)
+		if (tree->parent)
+			depth = 1 + binary_tree_depth(tree->parent);
 	return (depth);
 }
