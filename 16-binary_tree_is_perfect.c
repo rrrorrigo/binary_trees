8da3dec21de0f0651036291d255ea80019144519
RaimundoGallino <raigallino0@gmail.com>
2021-07-22 13:18:48 -0300

    fixed

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,7 +1,24 @@
 #include "binary_trees.h"
 #include <stdlib.h>
-#include "15-binary_tree_is_full.c"
-#include "14-binary_tree_balance.c"
+#include <stdio.h>
+#include "11-binary_tree_size.c"
+#include "9-binary_tree_height.c"
+/**
+ * _pow_recursion - check the code for Holberton School students.
+ * @x: base number
+ * @y: exponential number
+ * Return: the result
+ */
+size_t _pow_recursion(int x, int y)
+{
+	if (y == 0)
+	{
+		return (1);
+	}
+
+	return (x * _pow_recursion(x, y - 1));
+
+}
 /**
  * binary_tree_is_perfect - function that check if binary tree is perfect
  *
@@ -10,8 +27,13 @@
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
+
+	size_t height = binary_tree_height(tree) + 1;
+
 	if (!tree)
 		return (0);
-
-	return (binary_tree_is_full(tree) && !binary_tree_balance(tree));
+	if ((binary_tree_size(tree) + 1) == _pow_recursion(2, height))
+		return (1);
+	return (0);
+	
 }
