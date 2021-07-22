072c526c324ebf3e0814359e4cc1d34b2a00a1b2
Rodrigo <rodrigobriones54@gmail.com>
2021-07-22 00:30:03 +0000

    trying to fix an eror :(

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,4 +1,4 @@
-#include "binary_trees.h"
+ #include "binary_trees.h"
 #include <stdlib.h>
 /**
  * binary_tree_is_full - function that checks if a binary tree is full
@@ -8,18 +8,17 @@
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-	int r;
+        int l, r;
 
 	if (!tree)
 		return (0);
-	if (tree->left)
-		r = binary_tree_is_full(tree->left);
-	if (tree->right)
-		r = binary_tree_is_full(tree->right);
-	if (r == 0)
-		return (0);
-	if ((!tree->left && !tree->right) || (tree->left && tree->right))
+	if (!tree->left && !tree->right)
 		return (1);
-	else
-		return (0);
+	if (tree->left && tree->right)
+	{
+		l = binary_tree_is_full(tree->left);
+		r = binary_tree_is_full(tree->right);
+		return (l && r);
+	}
+	return (0);
 }
